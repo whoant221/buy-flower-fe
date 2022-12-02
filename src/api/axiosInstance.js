71349622df54360f110c9e4ede5c4004add0bcd0
whoant <vovanhoangtuan4.2@gmail.com>
2022-12-02 22:18:55 +0700

    Add domain

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://94.100.26.30:30001",
+    baseURL: "https://api-buy-flower.votuan.xyz",
     timeout: 30000,
     maxContentLength: 1000000,
     maxBodyLength: 1000000,
